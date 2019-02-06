30c5edd521feb31a1443c53b47c99406c97f3e1c
Cristiano <cristianovelkan@gmail.com>
2019-02-05 23:02:02 -0200

    fix exception

--- app/Http/Controllers/AddressController.php
+++ app/Http/Controllers/AddressController.php
@@ -2,47 +2,47 @@
 
 namespace App\Http\Controllers;
 
-use App\Address;
-use Illuminate\Http\Request;
-
-class AddressController extends Controller {
+class AddressController extends Controller
+{
 
     /**
      * Create new address.
      *
      * @return \Illuminate\Http\Response
      */
-    public static function index() {
+    public static function index()
+    {
         try {
-            
+
             $bitcoind = bitcoind()->getNewAddress();
             $address = json_decode($bitcoind->getBody());
-            
+
             return $address->result;
-            
+
         } catch (\Exception $ex) {
-            
-            throw new Exception($ex->getMessage());
+
+            throw new \Exception($ex->getMessage());
         }
     }
-    
+
 
     /**
      * Display info.
      *
-     * @param  \App\Address  $address
+     * @param  \App\Address $address
      * @return \Illuminate\Http\Response
      */
-    public static function show($address) {
+    public static function show($address)
+    {
         try {
-            
+
             $bitcoind = bitcoind()->getAddressInfo($address);
             $address = json_decode($bitcoind->getBody());
             return $address->result;
-            
+
         } catch (\Exception $ex) {
-            
-            throw new Exception($ex->getMessage());
+
+            throw new \Exception($ex->getMessage());
         }
     }
 
--- app/Http/Controllers/BalanceController.php
+++ app/Http/Controllers/BalanceController.php
@@ -6,25 +6,25 @@ use Illuminate\Http\Request;
 
 class BalanceController extends Controller
 {
-    
+
     /**
-     * 
+     *
      * Recupera o balance do core
      * @return type
      * @throws Exception
      */
     public static function getBalance(){
         try {
-            
+
             $bitcoind = bitcoind()->getBalance();
             $address = json_decode($bitcoind->getBody());
-            
+
             return $address->result;
-            
+
         } catch (\Exception $ex) {
-            
-            throw new Exception($ex->getMessage());
-        }        
+
+            throw new \Exception($ex->getMessage());
+        }
     }
 
 }
